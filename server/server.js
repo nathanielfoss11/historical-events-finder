682d0242f486aa9255998149b578385c4c7be27c
Nate Foss <nathanielfoss11@gmail.com>
2021-01-06 10:56:07 -0700

    Setup of initial Hello World repo

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('../client/dist'));
+app.use(express.json());
+
+
+app.get('', (req, res) => {
+		res.status(200).send('');
+});
+
+app.listen(PORT, () => {
+    console.log('Serving up now at '+ JSON.stringify(PORT))
+});
